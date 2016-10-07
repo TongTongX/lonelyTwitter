c78cdf4dd666f7f3dac098f552688eec64fe49b4
TongTong <alanzhaoxutong@hotmail.com>
2016-10-06 18:06:46 -0600

    javaDoc generated in /docs/

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2016 Team 20, CMPUT301, University of Alberta - All Rights Reserved.
+You may use, copy or distribute this code under terms and conditions of University of Alberta
+and Code of Student Behavior.
+Please contact abc@abc.ca for more details or questions.
+ */
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
@@ -24,14 +30,47 @@ import android.widget.ListView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+/**
+ * This class is the main view class in lonelyTwitter class.
+ * It deals with user inputs, saves/loads them in/from the file FILE_NAME (file.sav).
+ * <p> You can access this file from Android Device Monitor. </p>
+ * <pre> pre-formatted text</pre>
+ * <code>
+ *     pseudo-code that is used in this class is as follows:
+ *     step 1 <br>
+ *     step 2 <br>
+ * </code>
+ * <ol>
+ *     <li>first item</li>
+ *     <li>second item</li>
+ *     <li>third item</li>
+ * </ol>
+ * @author TongTong
+ * @since 1.4
+ * @see NormalTweet
+ * @see java.io.BufferedReader
+ * @see TweetList
+ */
 public class LonelyTwitterActivity extends Activity {
 
+	/**
+	 * This is the name of the file that is saved in your virtual device.
+	 * You can access it through Android Device Monitor by selecting your app.
+	 * then data -> data -> file.sav
+	 * @see NormalTweet
+	 * @author TongTong
+	 */
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
 	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
 	private ArrayAdapter<Tweet> adapter;
-	
+
+	/*
+	Testing multi-line documentations.
+	Testing.
+	 */
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -77,6 +116,11 @@ public class LonelyTwitterActivity extends Activity {
 		oldTweetsList.setAdapter(adapter);
 	}
 
+	/**
+	 * This method loads the json file and generates the tweets from its contents.
+	 * @throws RuntimeException
+	 * @exception FileNotFoundException
+	 */
 	private void loadFromFile() {
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
